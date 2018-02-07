5af4275b4763aaf35f33bee3c62b75ff9116969a
Michael <micalan@sas.upenn.edu>
2018-02-07 16:19:12 -0500

    read exp data file for block start/stop times in TRs

--- code/getExpParams.m
+++ code/getExpParams.m
@@ -1,32 +1,28 @@
-function [] = getExpParams(dataParamFile,TR)
+function [expParams] = getExpParams(dataParamFile,TR, varargin)
 
 
-
+%TR in s
 
 p = inputParser;
 p.addParameter('stripInitialTRs',1,@islogical);
-p.addParameter('dimensions',3,@isnumeric);
 p.parse(varargin{:});
 
 
 load(dataParamFile)
 
-protocolParams.trialTypeOrder
-
 %% load the stim file 
 
+experimentStart = responseStruct.events(1).tTrialStart;
 
 %% calculate stim duration
 for ii = 1: length(responseStruct.events)
-    blockTimes(ii) = (responseStruct.events(ii).tStimulusEnd - responseStruct.events(ii).tStimulusStart);
-end
-%% convert to tr index
-blockTRs = round(blockTimes./(func.tr/1000));
-
-%% bin 
-
-
+    expParams(ii,1) = round((responseStruct.events(ii).tStimulusStart - experimentStart)/TR)+1;
+    expParams(ii,2) = round((responseStruct.events(ii).tStimulusEnd - experimentStart)/TR);
+    expParams(ii,3) = protocolParams.trialTypeOrder(ii);
 end
 
+if stripInitialTRs
+   expParams = expParams + [2*ones(size(expParams,1),2) zeros(size(expParams,1),1)];
+end
 
 end
