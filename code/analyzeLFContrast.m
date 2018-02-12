6192f97a35b1eef126a8055a4fb64ebc76c3daa7
Michael <micalan@sas.upenn.edu>
2018-02-12 14:30:47 -0500

    working on analysis script

--- code/analyzeLFContrast.m
+++ code/analyzeLFContrast.m
@@ -4,13 +4,18 @@
 % LFContrast experiment.
 
 %% Set up params.
-subjID       = 'HEROgka1';
+subjID       = 'sub-HEROgka1';
 session      = 'ses-201709191435';
-funcRuns     = {'
+funcRuns     = {'sub-HEROgka1_ses-201709191435_task-tfMRILFContrastAP_run-1_bold_space-MNI152NLin2009cAsym_preproc.nii.gz', ...
+                'sub-HEROgka1_ses-201709191435_task-tfMRILFContrastAP_run-2_bold_space-MNI152NLin2009cAsym_preproc.nii.gz', ...
+                'sub-HEROgka1_ses-201709191435_task-tfMRILFContrastAP_run-3_bold_space-MNI152NLin2009cAsym_preproc.nii.gz', ...
+                'sub-HEROgka1_ses-201709191435_task-tfMRILFContrastPA_run-1_bold_space-MNI152NLin2009cAsym_preproc.nii.gz', ...
+                'sub-HEROgka1_ses-201709191435_task-tfMRILFContrastPA_run-2_bold_space-MNI152NLin2009cAsym_preproc.nii.gz', ...
+                'sub-HEROgka1_ses-201709191435_task-tfMRILFContrastPA_run-3_bold_space-MNI152NLin2009cAsym_preproc.nii.gz'};
 % Run applyRetAtlas2Functional
 
 % retinotopy file inputs
-inFiles = {'HERO_gka1_native.template_angle.nii.gz','HERO_gka1_native.template_areas.nii.gz','HERO_gka1_native.template_eccen.nii.gz'};
+inRetFiles = {'HERO_gka1_native.template_angle.nii.gz','HERO_gka1_native.template_areas.nii.gz','HERO_gka1_native.template_eccen.nii.gz'};
 % path to the retinotopy files
 path2input   = ['~/Documents/flywheel/retAtlas/',subjID];
 path2ref     = ['~/Documents/flywheel/fmriprep/',subjID,'/',session,'/func'];
@@ -18,34 +23,35 @@ refFileName  = 'sub-HEROgka1_ses-201709191435_task-tfMRILFContrastAP_run-1_bold_
 path2warp    = ['~/Documents/flywheel/fmriprep/', subjID, '/', session, '/anat'];
 warpFileName = 'sub-HEROgka1_ses-201709191435_T1w_space-MNI152NLin2009cAsym_warp.h5';
 
-for ii = 1:length(inFiles)
+for ii = 1:length(inRetFiles)
     % input file
-    inFile = fullfile(path2input,inFiles{ii});
+    inFile = fullfile(path2input,inRetFiles{ii});
     
     % output file
     [~,tempName,~] = fileparts(inFile);
     [~,outName,~] = fileparts(tempName);
-    outFile = fullfile(path2input,[outName '_MNI_resampled.nii.gz'])
+    outFile = fullfile(path2input,[outName '_MNI_resampled.nii.gz']);
     
     % reference file
     refFile = fullfile(path2ref,refFileName);
     
     % warp file
     warpFile = fullfile(path2warp,warpFileName);
-    
-    applyANTsWarpToData(inFile, outFile, warpFile, refFile)
+    if ~exist(outFile)
+        applyANTsWarpToData(inFile, outFile, warpFile, refFile)
+    end
 end
 
 %% Extract Signal from voxels
 % load ecc
-eccenPos = find(~cellfun(@isempty,strfind(inFiles,'eccen')));
-[~,tempName,~] = fileparts(inFiles{eccenPos});
+eccenPos = find(~cellfun(@isempty,strfind(inRetFiles,'eccen')));
+[~,tempName,~] = fileparts(inRetFiles{eccenPos});
 [~,outName,~] = fileparts(tempName);
 eccenOutFileName = fullfile(path2input,[outName '_MNI_resampled.nii.gz'])
 eccen = MRIread(eccenOutFileName);
 % load areas
-areasPos = find(~cellfun(@isempty,strfind(inFiles,'areas')));
-[~,tempName,~] = fileparts(inFiles{areasPos});
+areasPos = find(~cellfun(@isempty,strfind(inRetFiles,'areas')));
+[~,tempName,~] = fileparts(inRetFiles{areasPos});
 [~,outName,~] = fileparts(tempName);
 areasOutFileName = fullfile(path2input,[outName '_MNI_resampled.nii.gz'])
 areas = MRIread(areasOutFileName);
@@ -56,9 +62,6 @@ areasMap = areas.vol;
 
 areaVal   = 1; % 1 = v1 2 = v2 3 = v3
 eccenThresh = 30;
+funcRuns = strcat(path2ref,funcRuns);
+meanSignal = extractMeanSignalFromROI(funcRuns,areasMap,eccenMap,areaVal, eccenThresh)
 
-for 
-meanSignal = extractMeanSignalFromROI(timeSeries,areasMap,eccenMap,areaVal, eccenThresh)
-
-%% Plot the data
-figure; hold on
--- code/extractMeanSignalFromROI.m
+++ code/extractMeanSignalFromROI.m
@@ -1,13 +1,13 @@
-function meanSignal = extractMeanSignalFromROI(timeSeries,areaMap,eccMap,area, eccTresh)
+function meanSignal = extractMeanSignalFromROI(funcRuns,areaMap,eccMap,area, eccThresh)
 %extractMeanSignalFromROI -- extracts and plot the mean signal of an roi
 %
 % Inputs:
-%   timeSeries -- BOLD timeseries nifti volume.
+%   funcRuns -- BOLD timeseries nifti volume.
 %   ROI        -- The nifti file that tage the voxels of interest.
-%  
+%
 % Outputs:
 %   meanSignal -- The mean signal across voxels of the ROI per timepoint.
-% 
+%
 % Key Value Pairs:
 %   verbose    -- Plot the time course.
 %
@@ -16,3 +16,33 @@ function meanSignal = extractMeanSignalFromROI(timeSeries,areaMap,eccMap,area, e
 
 % MAB 2017 -- started function
 
+% clean up area map from the output of non linear warp
+if length(unique(areaMap)) ~= 4
+    areaMap(areaMap <0.7) = 0;
+    areaMap(areaMap > 0.75 & areaMap2 <=1.5) = 1;
+    areaMap(areaMap > 1.5 & areaMap <=2.25) = 2;
+    areaMap(areaMap > 2.25 & areaMap <=3.0) = 3;
+end
+
+% make mask
+areaMap = areaMap;
+areaMap(areaMap ~= area) = 0;
+areaMap(areaMap == area) = 1;
+areaMap = logical(areaMap);
+
+eccMap(eccMap <=  eccThresh) = 1; 
+eccMap(eccMap >  eccThresh) = 0; 
+eccMap = logical(eccMap);
+
+mask = areaMap & eccMap == 1;
+
+for ii = 1:length(funcRuns)
+    % load nifti for functional run
+    nii = MRIread(funcRuns{ii});
+    timeSeries = nii.vol;
+    
+    for jj = 1:size(timeSeries,4)
+        vol = timeSeries(:,:,:,jj);
+        meanSignal(jj,ii) = mean(vol(mask));
+    end
+end
