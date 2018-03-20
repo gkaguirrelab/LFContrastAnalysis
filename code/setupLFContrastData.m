6faa3031e74dc9274c7c45d44946293dfdeac6a7
gkaguirre <gkaguirre@me.com>
2018-03-20 12:46:50 -0400

    MAB to do this
    Signed-off-by: gkaguirre <gkaguirre@me.com>

---
+++ code/setupLFContrastData.m
@@ -0,0 +1,5 @@
+%% setupLFContrastData
+%
+% MAB: Write a script here to download the analysis products from flywheel
+% into the  directory specified in the local hook file and organize it in a
+% nice way
