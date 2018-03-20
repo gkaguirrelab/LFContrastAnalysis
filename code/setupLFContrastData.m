aded5f6e581243fcb5c236585ca955268bc9f802
Michael <micalan@sas.upenn.edu>
2018-03-20 15:03:35 -0400

    making bugs

--- code/setupLFContrastData.m
+++ code/setupLFContrastData.m
@@ -3,3 +3,23 @@
 % MAB: Write a script here to download the analysis products from flywheel
 % into the  directory specified in the local hook file and organize it in a
 % nice way
+
+%% Check for project level directory 
+projectDir = getpref('LFContrastAnalysis','projectRootDir');
+if ~exist(projectDir)
+    mkdir(projectDir)
+end
+
+%% Set up fmriprep directory and download
+
+fmriprepDir = fullfile(projectDir,'fmriprep')
+
+if ~exist(fmriprepDir)
+    mkdir(fmriprepDir)
+end
+
+% download the data 
+theProject    = 'LFContrast';
+analysisLabel = 'fmriprep 02/09/2018 11:40:55';
+analysis_id = getAnalysisFromFlywheel(theProject,analysisLabel,fmriprepDir, 'verbose', true)
+
