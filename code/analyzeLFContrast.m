f0abeb52df3446079813a671c105aef663720840
Michael <micalan@sas.upenn.edu>
2018-03-22 18:12:48 -0400

    analyzeLFContrast now works with prefs from local hook

--- code/analyzeLFContrast.m
+++ code/analyzeLFContrast.m
@@ -6,76 +6,77 @@
 %% Convenience variables
 projectName = 'LFContrastAnalysis';
 flywheelName = 'LFContrast';
+subjID       = 'sub-HEROgka1';
+session      = 'ses-201709191435';
 
 %% Analysis labels that we are going to go and get
 fmriprepLabel = 'fmriprep 02/09/2018 11:40:55';
 neuropythyLabel = 'retinotopy-templates 11/22/2017 13:21:46';
-[fwInfo] = getAnalysisFromFlywheel(flywheelName,fmriprepLabel,fmriprepDir, 'verbose', true, 'searchDir', projectDir, 'nodownload', true);
+[fwInfo] = getAnalysisFromFlywheel(flywheelName,fmriprepLabel,'', 'nodownload', true);
+sessionDir = fullfile(getpref('LFContrastAnalysis','projectRootDir'),[fwInfo.subject,'_', fwInfo.timestamp(1:10)]);
 
+%% Relevant Nifti names for analysis
 
-%% Set up params.
-subjID       = 'sub-HEROgka1';
-session      = 'ses-201709191435';
+% functional runs
 funcRuns     = {'sub-HEROgka1_ses-201709191435_task-tfMRILFContrastAP_run-1_bold_space-MNI152NLin2009cAsym_preproc.nii.gz', ...
                 'sub-HEROgka1_ses-201709191435_task-tfMRILFContrastAP_run-2_bold_space-MNI152NLin2009cAsym_preproc.nii.gz', ...
                 'sub-HEROgka1_ses-201709191435_task-tfMRILFContrastAP_run-3_bold_space-MNI152NLin2009cAsym_preproc.nii.gz', ...
                 'sub-HEROgka1_ses-201709191435_task-tfMRILFContrastPA_run-1_bold_space-MNI152NLin2009cAsym_preproc.nii.gz', ...
                 'sub-HEROgka1_ses-201709191435_task-tfMRILFContrastPA_run-2_bold_space-MNI152NLin2009cAsym_preproc.nii.gz', ...
                 'sub-HEROgka1_ses-201709191435_task-tfMRILFContrastPA_run-3_bold_space-MNI152NLin2009cAsym_preproc.nii.gz'};
+            
+% brain mask of function run for the reference volume in ANTs step
+refFileName  = 'sub-HEROgka1_ses-201709191435_task-tfMRILFContrastAP_run-1_bold_space-MNI152NLin2009cAsym_brainmask.nii.gz';
 
-%% Apply Warping to MNI space            
-% Set up vars in order to run applyANTsWarpToData
-
-% nifti input volumes (can be output of benson atlas or any nifti needed to
-% be tranformed into preproc space) 
+% output files of Neuropythy (retinotopy template)
 inRetFiles = {'HERO_gka1_native.template_angle.nii.gz','HERO_gka1_native.template_areas.nii.gz','HERO_gka1_native.template_eccen.nii.gz',};
 
-% path to the retinotopy files
-
-path2input   = ['~/Documents/flywheel/retAtlas/',subjID];
-path2input = fullfile(getpref(projectName,'projectRootDir'),'neuropythy');
-
-path2ref     = ['~/Documents/flywheel/fmriprep/',[subjID '_' ,'/',session,'/func'];
-refFileName  = 'sub-HEROgka1_ses-201709191435_task-tfMRILFContrastAP_run-1_bold_space-MNI152NLin2009cAsym_brainmask.nii.gz';
-path2warp    = ['~/Documents/flywheel/fmriprep/', subjID, '/', session, '/anat'];
+% warp file name (product of running fmriprep) 
 warpFileName = 'sub-HEROgka1_ses-201709191435_T1w_target-MNI152NLin2009cAsym_warp.h5';
+
+%% Apply Warping to MNI space
+
+% Set up vars in order to run applyANTsWarpToData
+retinoPath     = fullfile(sessionDir,'neuropythy');
+functionalPath = fullfile(sessionDir, 'fmriprep', subjID, session, 'func');
+warpFilePath   = fullfile(sessionDir, 'fmriprep', subjID, session, 'anat');
 
 % load ecc nifti file
 eccenPos = find(~cellfun(@isempty,strfind(inRetFiles,'eccen')));
 [~,tempName,~] = fileparts(inRetFiles{eccenPos});
 [~,outName,~] = fileparts(tempName);
-eccenFileName = fullfile(path2input,[outName '.nii.gz']);
+eccenFileName = fullfile(retinoPath,[outName '.nii.gz']);
 eccen = MRIread(eccenFileName);
 
 % load areas nifti file
 areasPos = find(~cellfun(@isempty,strfind(inRetFiles,'areas')));
 [~,tempName,~] = fileparts(inRetFiles{areasPos});
 [~,outName,~] = fileparts(tempName);
-areasFileName = fullfile(path2input,[outName,'.nii.gz']);
+areasFileName = fullfile(retinoPath,[outName,'.nii.gz']);
 areas = MRIread(areasFileName);
 
 % could add polar angle here but required current analysis
 areaNum = 1;
 eccenRange = [3 20];
-[~,maskSaveName] = makeMaskFromRetino(eccen,areas,areaNum,eccenRange,path2input);
+[~,maskSaveName] = makeMaskFromRetino(eccen,areas,areaNum,eccenRange,retinoPath);
 
 files2warp = {'HERO_gka1_T1.nii.gz',maskSaveName};
 
 for ii = 1:length(files2warp)
     % input file
-    inFile = fullfile(path2input,files2warp{ii});
+    inFile = fullfile(retinoPath,files2warp{ii});
     
     % output file
     [~,tempName,~] = fileparts(inFile);
     [~,outName,~] = fileparts(tempName);
-    outFile = fullfile(path2input,[outName '_MNI_resampled.nii.gz']);
+    outFile = fullfile(retinoPath,[outName '_MNI_resampled.nii.gz']);
     
     % reference file
-    refFile = fullfile(path2ref,refFileName);
+    refFile = fullfile(functionalPath,refFileName);
     
     
     % warp file
-    warpFile = fullfile(path2warp,warpFileName);
+    warpFile = fullfile(warpFilePath,warpFileName);
     if ~exist(outFile)
         applyANTsWarpToData(inFile, outFile, warpFile, refFile);
     end
@@ -85,12 +86,12 @@ end
 % Load mask nifti
 [~,tmpName,~] = fileparts(maskSaveName);
 [~,outName,~] = fileparts(tmpName);
-maskOutFileName = fullfile(path2input,[outName '_MNI_resampled.nii.gz']);
+maskOutFileName = fullfile(retinoPath,[outName '_MNI_resampled.nii.gz']);
 mask = MRIread(maskOutFileName);
 maskVol = mask.vol;
 
 % make full file path to funvtional runs
-funcRuns = fullfile(path2ref,funcRuns);
+funcRuns = fullfile(functionalPath,funcRuns);
 
 % extract the mean signal from voxels
 meanSignal = extractMeanSignalFromMask(funcRuns,maskVol);
