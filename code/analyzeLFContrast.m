d1bd68bd2a91838a6a6281d53e338c0d3e1fa6ab
David Brainard <brainard@psych.upenn.edu>
2018-09-27 16:30:32 -0400

    Comment style

--- code/analyzeLFContrast.m
+++ code/analyzeLFContrast.m
@@ -10,7 +10,7 @@ analysisParams.sessionNumber     = {'session_1','session_1'};
 analysisParams.sessionDir        = fullfile(getpref('LFContrastAnalysis','projectRootDir'),analysisParams.sessionFolderName);
 analysisParams.showPlots         = true;
 
-% brain mask of function run for the reference volume in ANTs step
+% Brain mask of function run for the reference volume in ANTs step
 analysisParams.refFileName  = 'sub-HEROGKA1_ses-ResearchAguirre_task-tfMRILFContrastAP_run-1_bold_space-MNI152NLin2009cAsym_brainmask.nii.gz';
 % output files of Neuropythy (retinotopy template)
 analysisParams.retinoFiles = {'HERO_gka1_native.template_angle.nii.gz','HERO_gka1_native.template_areas.nii.gz','HERO_gka1_native.template_eccen.nii.gz',};
@@ -21,7 +21,7 @@ analysisParams.warpFileName = 'sub-HEROGKA1_T1w_target-MNI152NLin2009cAsym_warp.
 % if no clipping then put 0;
 analysisParams.numClipFrames = 0;
 
-% make mask from the area and eccentricity maps
+% Make mask from the area and eccentricity maps
 analysisParams.areaNum     = 1;
 analysisParams.eccenRange  = [3 20];
 
@@ -31,14 +31,14 @@ analysisParams.baselineCondNum = 6;
 analysisParams.timeStep = 1/100;
 analysisParams.generateIAMPPlots = false;
 
-%Paramters for the QCM fit to IAMP:
+% Paramters for the QCM fit to IAMP:
 analysisParams.contrastCoding = [1, .5, .25, .125, .0625];
 analysisParams.LMVectorAngles = [ -45, 45, 0, 90, -22.5, 22.5, 67.5, 112.5]; 
 analysisParams.directionCoding = vectorAngle2LMScontrast(analysisParams.LMVectorAngles,'LM'); 
 analysisParams.maxContrastPerDir = [0.06,0.40,0.10,0.10,0.085,0.24,0.20,0.10]; % max contrast in the same order as above
 analysisParams.theDimension = 2;
 
- %plotting params
+% Plotting params
  analysisParams.numSamples = 25;
 
 % Get the cleaned time series
@@ -47,10 +47,10 @@ analysisParams.theDimension = 2;
 % Run the IAMP/QCM model
 [analysisParams,paramsQCMFit, meanIAMPBetas, semIAMPBetas,packetPocket,paramsFitIAMP,fitResponseStructQCM] = runIAMP_QCM(analysisParams,fullCleanData);
 
-%% Plot the CRF from the IAMP and QCM fits
+% Plot the CRF from the IAMP and QCM fits
 plotIAMP_QCM_CRF(analysisParams,meanIAMPBetas,semIAMPBetas,paramsQCMFit);
 
-%% Plot isoresponce contour
+% Plot isoresponce contour
 thresholds = [.25, .5, .75];
 hdl = plotIsoresponse(analysisParams,meanIAMPBetas,paramsQCMFit,thresholds);
 
