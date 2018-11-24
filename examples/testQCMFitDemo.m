dc8594e423a5da5d2dfe710c16beb7712ca518f5
David Brainard <brainard@psych.upenn.edu>
2018-11-24 14:46:59 -0500

    Change to match new row/col and other conventions on tfeQCM and BrainardLab dev branches.

--- examples/testQCMFitDemo.m
+++ examples/testQCMFitDemo.m
@@ -12,20 +12,18 @@
 
 % History:
 %   11/20/18  dhb, mab  Tuned this up; things are making sense.
+%   1//24/18  dhb       A bit more tuning.  Fix row/col convention.
 
-%--------------------------------------------------------------------------
 %% Initialize
-%--------------------------------------------------------------------------
 clear; close all
 
-%--------------------------------------------------------------------------
 %% Set up params
-%--------------------------------------------------------------------------
+%
 % Naka Ruston Params
-NOOFFSET = true;
+NOOFFSET = false;
 theDimension = 2;
-Rmax   = .9;
-sigma  = .7;
+Rmax   = 0.9;
+sigma  = 0.7;
 n      = 2.1;
 if (NOOFFSET)
     offset = 0;
@@ -33,21 +31,20 @@ else
     offset = -0.1;
 end
 
-% Ellipse Params
-minorAxis = .3;
+% Ellipse parameters
+minorAxis = 0.3;
 rotdeg    = 45;
+ellParams = [1 minorAxis rotdeg];
 
-% Param Vec
-ellParams = [1, minorAxis, rotdeg];
-
-%--------------------------------------------------------------------------
 %% Generate Stimulus:
-%--------------------------------------------------------------------------
+%
 % Random stimuli bounded between 1 and -1
 RANDOM_STIMULI = false;
 if (RANDOM_STIMULI)
     numStim = 300;
     stim = (2*rand(2,numStim) -1);
+    
+% Stimuli that vary along specified directions
 else
     maxContrast = 0.6;
     contrastCoding = [0.0625, 0.125, 0.25, 0.5, 1];
@@ -61,9 +58,11 @@ else
     numStim = size(stim,2);
 end
 
-%--------------------------------------------------------------------------
-%% Generate Response:
-%--------------------------------------------------------------------------
+%% Generate response
+% This does by hand what our compute response routine does, and is here
+% just to expose that calculation and make sure it does what we think it
+% should.
+%
 % Set up scale matrix
 S = diag(ellParams(1:2));
 
@@ -71,23 +70,22 @@ S = diag(ellParams(1:2));
 %
 % Whether or not we tag on a transpose here defines 
 % the convention for angle. Currently matched to the
-% TFE implementation, may want to clarify and/or change.
-V = deg2rotm(ellParams(3));  
+% tfeQCM implementation and seems like the right convention
+% for the two-dimensional case at least.
+V = deg2rotm(ellParams(3))';  
 
-% Get the Q matrix that takes stim and get a radius. 
+% Get the Q matrix that takes stimulus and get a radius. 
 A = S*V';
 Q = A'*A;
 
 % Get the radius
-radius =  diag(sqrt(stim'*Q*stim));
+radius = diag(sqrt(stim'*Q*stim));
 
 % Get the neural response values
-R  = nakaRushton(radius,sigma,n,Rmax, offset);
+R  = nakaRushton(radius,sigma,n,Rmax,offset);
 
-%--------------------------------------------------------------------------
 %% Let's check that the QCM forward model gives the same responses.
-%--------------------------------------------------------------------------
-
+%
 % Construct the model object 
 tfeResponseCheck = tfeQCM('verbosity','none','dimension',theDimension);
 stimulusStruct.values = stim;
@@ -107,10 +105,9 @@ if (max(abs(R'-modelResponseStruct.values)) > 1e-15)
     error('Hand computation of QCM model does not match tfeQCM forward model');
 end
 
-%--------------------------------------------------------------------------
-%%  Use the tfeQCM to fit the stim/resp:
-%--------------------------------------------------------------------------
 
+%%  Use the tfeQCM to fit the stim/resp:
+%
 % Get the tfeQCM object
 temporalFitQCM = tfeQCM('verbosity','none','dimension',theDimension);
 
@@ -137,15 +134,14 @@ end
 fprintf('Model parameter from fits:\n');
 temporalFitQCM.paramPrint(paramsQCMFit)
 
-%--------------------------------------------------------------------------
 %%  Check that the fit recovers the responses we put in
-%--------------------------------------------------------------------------
-
+% This is done by hand to match hand-coded method above,
+% but could be done by a call through the tfeQCM routine.
 qcmParams =[1 paramsQCMFit.Qvec];
 S_qcm = diag(qcmParams(1:2));
 
 % Set up rotation matrix
-V_qcm = deg2rotm(qcmParams(3));  
+V_qcm = deg2rotm(qcmParams(3))';  
 
 % Get the Q matrix that takes stim and get a radius. 
 A_qcm = S_qcm*V_qcm';
@@ -154,18 +150,17 @@ Q_qcm = A_qcm'*A_qcm;
 % Get the raduis
 radiusQcm =  diag(sqrt(stim'*Q_qcm*stim));
 
-% Get the predicted response values.  These should match the responses
-% we simulated above.  But note that there are some ambiguities in the
-% parameterization of the QCM model, because of a +/- 90 degree ambiguit
-% about which is the major and which is the minor axis of the ellipse.
+% Get the predicted response values.  These should match reasonably well
+% the responses we simulated above.  But note that there are some
+% ambiguities in the parameterization of the QCM model, because of a +/- 90
+% degree ambiguit about which is the major and which is the minor axis of
+% the ellipse.
 Rqcm  = nakaRushton(radiusQcm,paramsQCMFit.crfSemi,paramsQCMFit.crfExponent,paramsQCMFit.crfAmp, paramsQCMFit.offset);
-if (max(abs(R-Rqcm)/max(abs(R))) > 1e-3)
+if (max(abs(R-Rqcm)/max(abs(R))) > 1e-2)
     error('Hand computation of QCM model does not match tfeQCM forward model');
 end
 
-%--------------------------------------------------------------------------
-%%  Plot
-%--------------------------------------------------------------------------
+%%  Plot simulated and predicted responses
 figure; hold on
 p1 = scatter3(stim(1,:),stim(2,:),radius,'b','*');
 p2 = scatter3(stim(1,:),stim(2,:),radiusQcm,'r');
@@ -185,9 +180,7 @@ zlabel('Response')
 title('The response as found by passing the radius throught the naka rushton');
 legend([p1 p2], 'original', 'QCM recovered')
 
-%--------------------------------------------------------------------------
-%%  Check Naka-Rushton inverts
-%--------------------------------------------------------------------------
+%%  Check that Naka-Rushton funciton inverts
 thresh = 0.3;
 eqContrast = InvertNakaRushton([paramsQCMFit.crfAmp,paramsQCMFit.crfSemi,paramsQCMFit.crfExponent],thresh);
 circlePoints = eqContrast*UnitCircleGenerate(numStim);
@@ -198,22 +191,34 @@ if (any(abs(checkThresh-thresh) > 1e-10))
     error('Did not invert QCM model correctly');
 end
 
-%--------------------------------------------------------------------------
 %%  Find contrast in given direction that produces desired response
-%--------------------------------------------------------------------------
 if (~RANDOM_STIMULI)
+    maxResponseFactor = 3;
     whichDirection = 2;
-    theDirection = theDirections(:,whichDirection)';
+    
+    % Pull out this direction and simulated responses
+    theDirection = theDirections(:,whichDirection);
     directionResponses = R(nContrastsPerDirection*(whichDirection-1)+1:nContrastsPerDirection*whichDirection);
-    figure; hold on
-    plot(maxContrast*contrastCoding,directionResponses,'ro','MarkerFaceColor','r','MarkerSize',12);
-    xlabel('Contrast'); ylabel('Response');
     maxResponse = max(directionResponses);
     
-    [contrastFromSim,stimulusFromSim] = tfeQCMInvert(params1,theDirection,maxResponse/2);
-    plot(contrastFromSim,maxResponse/2,'bo','MarkerFaceColor','b','MarkerSize',8);
-    [contrastFromFit,stimulusFromFit] = tfeQCMInvert(paramsQCMFit,theDirection,maxResponse/2);
-    plot(contrastFromFit,maxResponse/2,'gx','MarkerSize',14);
+    % Invert model for chosen direction
+    [contrastFromSim,stimulusFromSim] = tfeQCMInvert(params1,theDirection,maxResponse/maxResponseFactor);
+    [contrastFromFit,stimulusFromFit] = tfeQCMInvert(paramsQCMFit,theDirection,maxResponse/maxResponseFactor);
+
+    % Plot simulated CRF and inverted points
+    figure; hold on
+    plot(maxContrast*contrastCoding,directionResponses,'ro','MarkerFaceColor','r','MarkerSize',12);
+    plot(contrastFromSim,maxResponse/maxResponseFactor,'bo','MarkerFaceColor','b','MarkerSize',8);
+    plot(contrastFromFit,maxResponse/maxResponseFactor,'gx','MarkerSize',14); 
+    xlabel('Contrast'); ylabel('Response');
     
+    % Plot an isoresponse contour of the simualted and fit model
+    nTheta = 100;
+    directions = UnitCircleGenerate(nTheta);
+    [contrasts1,stimuli1] = tfeQCMInvert(params1,directions,params1.crfAmp/3);
+    figure; hold on
+    plot(stimuli1(1,:),stimuli1(2,:),'r','LineWidth',3);
+    [contrastsFit,stimuliFit] = tfeQCMInvert(paramsQCMFit,directions,params1.crfAmp/3);
+    plot(stimuliFit(1,:),stimuliFit(2,:),'b','LineWidth',2);
 end
 
