25f5f6577fd11cdca8bf11b3a8b82d6a87f14fe2
Michael <micalan@sas.upenn.edu>
2020-03-16 13:13:44 -0400

    flag to turn off NR fits

--- code/analyzeLFContrast.m
+++ code/analyzeLFContrast.m
@@ -1,15 +1,15 @@
 %% Set up params
 % Set the subject: 'LZ23', 'KAS25', 'AP26'
-subjId = 'KAS25'; 
+subjId = 'KAS25';
 
 % Number of bootstrap iterations
-numIter  = 100; 
+numIter  = 100;
 
 % Load the subject relevant info
 analysisParams = getSubjectParams(subjId);
 
-% Flag for running all the NR models 
-analysisParams.runAllModels = false;
+% Flag for running all the NR models
+analysisParams.runNRModels = false;
 
 %set the preprocessing method that was used to ananlyze the data.
 analysisParams.preproc = 'hcp';
@@ -25,7 +25,7 @@ analysisParams.showPlots = true;
 % Load the time course
 [fullCleanData, analysisParams] = getTimeCourse_hcp(analysisParams);
 
-%% Get a packet for each run (1-20) 
+%% Get a packet for each run (1-20)
 [analysisParams, iampTimeCoursePacketPocket] = generateRunPackets(analysisParams, fullCleanData);
 
 %% Fit the GLM to get the crf betas
@@ -41,19 +41,19 @@ defaultParamsInfo.nInstances = size(theFullPacket.stimulus.values,1);
 
 %% Bootstrap
 % init vars
-paramsMat   = []; 
+paramsMat   = [];
 for ii = 1:numIter
-[analysisParams, theFullPacketBoot] = concatPackets(analysisParams, iampTimeCoursePacketPocket,'bootstrap',true);
-
-% fit the IAMP model
-defaultParamsInfo.nInstances = size(theFullPacket.stimulus.values,1);
-[iampParamsBoot,fValBoot(ii),~] = iampOBJ.fitResponse(theFullPacketBoot,...
-    'defaultParamsInfo', defaultParamsInfo, 'searchMethod','linearRegression');
-paramsMat = [paramsMat, iampParamsBoot.paramMainMatrix];
+    [analysisParams, theFullPacketBoot] = concatPackets(analysisParams, iampTimeCoursePacketPocket,'bootstrap',true);
+    
+    % fit the IAMP model
+    defaultParamsInfo.nInstances = size(theFullPacket.stimulus.values,1);
+    [iampParamsBoot,fValBoot(ii),~] = iampOBJ.fitResponse(theFullPacketBoot,...
+        'defaultParamsInfo', defaultParamsInfo, 'searchMethod','linearRegression');
+    paramsMat = [paramsMat, iampParamsBoot.paramMainMatrix];
 end
 
 % Calc SEM for betas
-semIAMP = std(paramsMat,0,2)./sqrt(numIter);
+semIAMP = std(paramsMat,0,2);
 semParams = iampParams;
 semParams.paramMainMatrix =semIAMP;
 
@@ -64,25 +64,29 @@ timeCoursePacket = makeDirectionTimeCoursePacketPocket({theFullPacket});
 % Make the CRF packet
 directionCrfMeanPacket = makeDirectionCrfPacketPocket(analysisParams,iampParams);
 
-%% Fit the direction based models to the mean IAMP beta weights
-% Fit the CRF -- { } is because this expects a cell
-[nrCrfOBJ,nrCrfParams] = fitDirectionModel(analysisParams, 'nrFit', timeCoursePacket);
+%% Fit the direction based models to the concatenated time course
 
-% Fit the CRF with the NR common amplitude -- { } is because this expects a cell
-[~,nrCrfParamsAmp] = fitDirectionModel(analysisParams, 'nrFit', timeCoursePacket, 'commonAmp', true);
+% run NR models if set to true
+if analysisParams.runNRModels
+    % Fit the CRF -- { } is because this expects a cell
+    [nrCrfOBJ,nrCrfParams] = fitDirectionModel(analysisParams, 'nrFit', timeCoursePacket);
+    
+    % Fit the CRF with the NR common amplitude -- { } is because this expects a cell
+    [~,nrCrfParamsAmp] = fitDirectionModel(analysisParams, 'nrFit', timeCoursePacket, 'commonAmp', true);
+    
+    % Fit the CRF with the NR common Exponent -- { } iPs because this expects a cell
+    [~,nrCrfParamsExp] = fitDirectionModel(analysisParams, 'nrFit', timeCoursePacket, 'commonExp', true);
+    
+    % Fit the CRF with the NR common amplitude, and exponent  -- { } is because this expects a cell
+    [~,nrCrfParamsAmpExp] = fitDirectionModel(analysisParams, 'nrFit', timeCoursePacket, 'commonAmp', true, 'commonExp', true);
+end
 
-% Fit the CRF with the NR common Exponent -- { } iPs because this expects a cell
-[~,nrCrfParamsExp] = fitDirectionModel(analysisParams, 'nrFit', timeCoursePacket, 'commonExp', true);
-
-% Fit the CRF with the NR common amplitude, and exponent  -- { } is because this expects a cell
-[~,nrCrfParamsAmpExp] = fitDirectionModel(analysisParams, 'nrFit', timeCoursePacket, 'commonAmp', true, 'commonExp', true);
-
-% Fit the CRF with the QCM -- { } is because this expects a cell
-[qcmCrfMeanOBJ,qcmCrfMeanParams] = fitDirectionModel(analysisParams, 'qcmFit', timeCoursePacket,'fitErrorScalar',1000);
+% Fit the time course with the QCM -- { } is because this expects a cell
+[qcmCrfOBJ,qcmCrfParams] = fitDirectionModel(analysisParams, 'qcmFit', timeCoursePacket,'fitErrorScalar',1000);
 
 % Do some plotting of these fits
 if analysisParams.showPlots
-    [nrVals] = plotNakaRushtonFromParams(qcmCrfMeanParams{1}.crfAmp ,qcmCrfMeanParams{1}.crfExponent,qcmCrfMeanParams{1}.crfSemi,...
+    [nrVals] = plotNakaRushtonFromParams(qcmCrfParams{1}.crfAmp ,qcmCrfParams{1}.crfExponent,qcmCrfParams{1}.crfSemi,...
         'analysisParams',analysisParams,'plotFunction',true,'savePlot',true);
 end
 
@@ -90,25 +94,26 @@ end
 crfStimulus = upsampleCRF(analysisParams);
 
 % Predict CRF from direction model fits
-
-% Predict the responses for CRF with params from NR common Amp.
-crfPlot.respNrCrf = nrCrfOBJ.computeResponse(nrCrfParams{1},crfStimulus,[]);
-crfPlot.respNrCrf.color = [.5, .3, .8];
-
-% Predict the responses for CRF with params from NR common Amp.
-crfPlot.respNrCrfAmp = nrCrfOBJ.computeResponse(nrCrfParamsAmp{1},crfStimulus,[]);
-crfPlot.respNrCrfAmp.color = [0, 0, 1];
-
-% Predict the responses for CRF with params from NR common Exp
-crfPlot.respNrCrfExp = nrCrfOBJ.computeResponse(nrCrfParamsExp{1},crfStimulus,[]);
-crfPlot.respNrCrfExp.color = [0, .33, 1];
-
-% Predict the responses for CRF with params from NR common Amp and Exps
-crfPlot.respNrCrfAmpExp = nrCrfOBJ.computeResponse(nrCrfParamsAmpExp{1},crfStimulus,[]);
-crfPlot.respNrCrfAmpExp.color = [0, .66, 1];
+if analysisParams.runNRModels
+    % Predict the responses for CRF with params from NR common Amp.
+    crfPlot.respNrCrf = nrCrfOBJ.computeResponse(nrCrfParams{1},crfStimulus,[]);
+    crfPlot.respNrCrf.color = [.5, .3, .8];
+    
+    % Predict the responses for CRF with params from NR common Amp.
+    crfPlot.respNrCrfAmp = nrCrfOBJ.computeResponse(nrCrfParamsAmp{1},crfStimulus,[]);
+    crfPlot.respNrCrfAmp.color = [0, 0, 1];
+    
+    % Predict the responses for CRF with params from NR common Exp
+    crfPlot.respNrCrfExp = nrCrfOBJ.computeResponse(nrCrfParamsExp{1},crfStimulus,[]);
+    crfPlot.respNrCrfExp.color = [0, .33, 1];
+    
+    % Predict the responses for CRF with params from NR common Amp and Exps
+    crfPlot.respNrCrfAmpExp = nrCrfOBJ.computeResponse(nrCrfParamsAmpExp{1},crfStimulus,[]);
+    crfPlot.respNrCrfAmpExp.color = [0, .66, 1];
+end
 
 % Predict the responses for CRF with params from QCM
-crfPlot.respQCMCrf = qcmCrfMeanOBJ.computeResponse(qcmCrfMeanParams{1},crfStimulus,[]);
+crfPlot.respQCMCrf = qcmCrfOBJ.computeResponse(qcmCrfParams{1},crfStimulus,[]);
 crfPlot.respQCMCrf.color = [0, 1, 0];
 
 % Plot the CRF from the IAMP, QCM, and  fits
@@ -121,20 +126,22 @@ end
 
 % Get the time course predicitions of the CRF params
 
-% Get the time course predicitions from the NR common Amp and Semi fit to the CRF
-nrAmp = responseFromPacket('nrFullTCPred', analysisParams, nrCrfParamsAmp{1}, timeCoursePacket, 'plotColor', [0, 0, 1]);
-[timeCoursePlot.nrAmp] = chopUpTimeCourse(nrAmp{1},20);
-
-% Get the time course predicitions from the NR common Amp and Semi fit to the CRF
-nrAmpSemi = responseFromPacket('nrFullTCPred', analysisParams, nrCrfParamsExp{1}, timeCoursePacket, 'plotColor', [0, .33, 1]);
-[timeCoursePlot.nrAmpSemi] = chopUpTimeCourse(nrAmpSemi{1},20);
-
-% Get the time course predicitions from the NR common Amp and Semi fit to the CRF
-nrAmpSemiExp = responseFromPacket('nrFullTCPred', analysisParams, nrCrfParamsAmpExp{1}, timeCoursePacket, 'plotColor', [0, .66, 1]);
-[timeCoursePlot.nrAmpSemiExp] = chopUpTimeCourse(nrAmpSemiExp{1},20);
+if analysisParams.runNRModels
+    % Get the time course predicitions from the NR common Amp and Semi fit to the CRF
+    nrAmp = responseFromPacket('nrFullTCPred', analysisParams, nrCrfParamsAmp{1}, timeCoursePacket, 'plotColor', [0, 0, 1]);
+    [timeCoursePlot.nrAmp] = chopUpTimeCourse(nrAmp{1},20);
+    
+    % Get the time course predicitions from the NR common Amp and Semi fit to the CRF
+    nrAmpSemi = responseFromPacket('nrFullTCPred', analysisParams, nrCrfParamsExp{1}, timeCoursePacket, 'plotColor', [0, .33, 1]);
+    [timeCoursePlot.nrAmpSemi] = chopUpTimeCourse(nrAmpSemi{1},20);
+    
+    % Get the time course predicitions from the NR common Amp and Semi fit to the CRF
+    nrAmpSemiExp = responseFromPacket('nrFullTCPred', analysisParams, nrCrfParamsAmpExp{1}, timeCoursePacket, 'plotColor', [0, .66, 1]);
+    [timeCoursePlot.nrAmpSemiExp] = chopUpTimeCourse(nrAmpSemiExp{1},20);
+end
 
 % Get the time course predicitions fromt the QCM params fit to the CRF
-qcmTimeCourse = responseFromPacket('qcmPred', analysisParams, qcmCrfMeanParams{1}, timeCoursePacket, 'plotColor', [0, 1, 0]);
+qcmTimeCourse = responseFromPacket('qcmPred', analysisParams, qcmCrfParams{1}, timeCoursePacket, 'plotColor', [0, 1, 0]);
 [timeCoursePlot.qcm] = chopUpTimeCourse(qcmTimeCourse{1},20);
 
 % Add the IAMP
@@ -145,11 +152,6 @@ iampResponses.plotColor = [.5,.5,.5];
 theTimeCourse = {theFullPacket.response};
 theTimeCourse{1}.plotColor =[0, 0, 0];
 [timeCoursePlot.timecourse] = chopUpTimeCourse(theTimeCourse{1},20);
-
-
-%% plot stuff
-
-
 
 % Plot the time course prediction for each run using the different fits to
 % the crf
