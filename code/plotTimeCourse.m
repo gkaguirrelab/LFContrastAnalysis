aab15aaa38ecbc2a48b755f3528de520bf723f51
Michael <micalan@sas.upenn.edu>
2020-03-27 19:40:45 -0400

    added R^2 valeus to the TC plot

--- code/plotTimeCourse.m
+++ code/plotTimeCourse.m
@@ -46,30 +46,48 @@ for ii = 1:numSubPlots
     
     for jj = 1:length(fields)
         
+        % get the sturct
         theModelResp = eval(['timeCoursePlot.', fields{jj}]);
         
-        
+        % get the time course
         if strcmp('timecourse',fields{jj})
             response = theModelResp{ii}.values;
+            timeCourse = response;
+        elseif strcmp('qcm',fields{jj})
+            response = theModelResp{ii}.values + baselineShift(ii);
+            qcmPred = response;
+        elseif strcmp('IAMP',fields{jj})
+            response = theModelResp{ii}.values + baselineShift(ii);
+            iampResp = response;
         else
             response = theModelResp{ii}.values + baselineShift(ii);
-            
         end
         
         subplot(rws,cols,ii); hold on
         if isfield(theModelResp{ii}, 'shaddedErrorBars')
             shadedErrorBars(theModelResp{ii}.timebase,response,theModelResp{ii}.shaddedErrorBars,...
-                'lineprops',{'color',theModelResp{ii}.plotColor},'patchSaturation', 0.35);
+                'lineprops',{'color',theModelResp{ii}.plotColor},'patchSaturation', 0.25);
         end
         p(jj) = plot(theModelResp{ii}.timebase,response,'color',theModelResp{ii}.plotColor,'LineWidth', 1.0);
         
-        % put info
-        ylabel('PSC')
-        xlabel('Time mS')
-        title(sprintf('Run = %s', num2str(ii)));
-        set(gca, 'FontName', 'Helvetica', 'FontSize', 14,'FontWeight', 'normal');
+        
         
     end
+    
+    if ~isempty(find(strcmp(fields(:), 'qcm')))
+        tempR2 = corr([timeCourse',qcmPred']).^2;
+        qcmR2 = tempR2(1,2);
+    end
+    if ~isempty(find(strcmp(fields(:), 'IAMP')))
+        tempR2 = corr([timeCourse',iampResp']).^2;
+        iampR2 = tempR2(1,2);
+    end
+    
+    % put info
+    ylabel('PSC')
+    xlabel('Time mS')
+    title(sprintf('Run %s: QCM R^{2} %s, IAMP R^{2} %s', num2str(ii),num2str(round(qcmR2,2)),num2str(round(iampR2,2))));
+    set(gca, 'FontName', 'Helvetica', 'FontSize', 14,'FontWeight', 'normal');
 end
 
 
