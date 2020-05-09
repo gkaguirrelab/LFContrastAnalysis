c209350d90400f93e98da164c7b3eeaa2640ceb2
Michael <micalan@sas.upenn.edu>
2020-05-09 14:44:29 -0400

    adding our stim

--- code/checkVisualFieldSizeEffect.m
+++ code/checkVisualFieldSizeEffect.m
@@ -6,7 +6,7 @@ load('PTB3TestCal.mat','cals');
 % Extract the spectral power distributions of the display's RGB primaries
 displaySPDs = (calStructOBJ.get('P_device'))';
 
-% Get the cone fundamentals 
+% Get the cone fundamentals
 S = calStructOBJ.get('S');
 wavelengthAxis = SToWls(S);
 [~,tempFundamentals] = ComputeCIEConeFundamentals(S,2,30,3);
@@ -17,8 +17,8 @@ coneFundamentals = T_energy./max(T_energy')';
 backgroundPrimaries = [0.5 0.5 0.5]';
 
 % Speficy stimulus contrast
-LminusM_Modulation = [0.06 -0.06 0]';
-LplusM_Modulation = [0.4 0.4 0]';
+LminusM_Modulation = [0.065 -0.065 0]';
+LplusM_Modulation = [0.41 0.41 0]';
 
 % Compute cone excitations for the background
 backgroundSPD = backgroundPrimaries(1) * displaySPDs(1,:) + ...
@@ -42,63 +42,11 @@ LplusM_Primaries = M\LplusM_Excitations;
 LminusM_SPDs = sum(displaySPDs.*LminusM_Primaries,1);
 LplusM_SPDs = sum(displaySPDs.*LplusM_Primaries,1);
 
-% % Plot it
-% figure; hold on
-% plot(wavelengthAxis,backgroundSPD,'k','LineWidth',2)
-% plot(wavelengthAxis,LminusM_SPDs,'r--','LineWidth',2)
-% plot(wavelengthAxis,LplusM_SPDs,'b--','LineWidth',2)
-% legend('background','L-M','L+M')
-% xlabel('Wavelength (nm)')
-% ylabel('Power')
-
 % Visual Feild Size Spacing
 fieldSizes = 1:20;
 
-% Loop over visual field sizes
-for ii = 1:length(fieldSizes)
-    
-    % Generate cone fundamentals for different visual field size
-    [~,T_quantal] = ComputeCIEConeFundamentals(S,fieldSizes(ii),30,3);
-    T_energy = EnergyToQuanta(S,T_quantal')';
-    cieConeFund = T_energy./max(T_energy')';
-    
-    % Compute the background activations
-    bkgrd = cieConeFund * backgroundSPD';
-    
-    % compute the L-M activations
-    LminusM_Activations = cieConeFund * LminusM_SPDs';
-    
-    % Compute the L+M activations
-    LplusM_Activations = cieConeFund * LplusM_SPDs';
-    
-    % Compute L-M Contrast
-    LminusM_Contrast(:,ii) = (LminusM_Activations - bkgrd) ./bkgrd;
-    
-    % Compute L+M Contrast
-    LplusM_Contrast(:,ii)  = (LplusM_Activations - bkgrd) ./bkgrd;
-    
-end
-
-% Turn the contrasts from above loop into inputs to the QCM
-% L-M direction
-% Get the angle and contrast
-[theta, contrast] = cart2pol(LminusM_Contrast(1,:),LminusM_Contrast(2,:));
-
-% X and y component for a unit vector in the direction of theta
-[LconesCont, MConesCont] = pol2cart(theta, 1);
-
-% package for the QCM stim
-LminusM_QcmStim = [LconesCont;MConesCont;contrast];
-
-% L+M direction
-% Get the angle and contrast
-[theta, contrast] = cart2pol(LplusM_Contrast(1,:),LplusM_Contrast(2,:));
-
-% X and y component for a unit vector in the direction of theta
-[LconesCont, MConesCont] = pol2cart(theta, 1);
-
-% package for the QCM stim
-LplusM_QcmStim = [LconesCont;MConesCont;contrast];
+[LminusM_QcmStim] = computeContrastStimWithCIE(S,fieldSizes,backgroundSPD,LminusM_SPDs);
+[LplusM_QcmStim] = computeContrastStimWithCIE(S,fieldSizes,backgroundSPD,LplusM_SPDs);
 
 % Run the QCM for KAS25
 analysisParams = getSubjectParams('KAS25');
@@ -109,7 +57,7 @@ params.Qvec        = [0.1661 46.1730];
 params.crfAmp      = 2.9998;
 params.crfExponent = 1.4826;
 params.crfSemi     = 0.2817;
-params.expFalloff  = 5.0501; % this does nothing and should be removed 
+params.expFalloff  = 5.0501; % this does nothing and should be removed
 params.crfOffset   = -0.1737;
 
 % Create the Stimulus Struct for L-M
@@ -120,7 +68,7 @@ stimulusStruct_LminusM.timebase = 1:length(fieldSizes);
 stimulusStruct_LplusM.values = LplusM_QcmStim;
 stimulusStruct_LplusM.timebase = 1:length(fieldSizes);
 
-% Get the HRF 
+% Get the HRF
 kernel = [];
 
 % Compute the L-M QCM response
@@ -132,7 +80,76 @@ LplusM_Response = fitOBJ.computeResponse(params,stimulusStruct_LplusM,kernel);
 % Compute the ratio of the L
 LMratio = LminusM_Response.values ./ LplusM_Response.values;
 
-% Plot some stuff
+
+%% The nominal stimuli from our exteriment
+
+% Load measuremets from session.
+
+% Get the SDPs
+directionNum = 1;
+bkgrdSPD = ConeDirectedDirections{directionNum}.describe.backgroundNominal.SPDdifferentialDesired;
+LminusM_SPD_pos = ConeDirectedDirections{directionNum}.describe.backgroundNominal.SPDdifferentialDesired+ConeDirectedDirections{directionNum}.SPDdifferentialDesired(:,1);
+
+directionNum = 3;
+LplusM_SPD_pos = ConeDirectedDirections{directionNum}.describe.backgroundNominal.SPDdifferentialDesired+ConeDirectedDirections{directionNum}.SPDdifferentialDesired(:,1);
+
+% Make the Cone fundamentals
+wavelengthAxis = SToWls(ConeDirectedDirections{directionNum}.calibration.describe.S);
+[~,tempFundamentals] = ComputeCIEConeFundamentals(ConeDirectedDirections{directionNum}.calibration.describe.S,2,30,3);
+T_energy = EnergyToQuanta(ConeDirectedDirections{directionNum}.calibration.describe.S,tempFundamentals')';
+coneFundamentals = T_energy./max(T_energy')';
+
+%
+backgroundExcitations = coneFundamentals * bkgrdSPD;
+stimExcitations = coneFundamentals * LminusM_SPD_pos;
+contrast = (stimExcitations-backgroundExcitations)./backgroundExcitations
+norm(contrast(1:2))
+
+
+stimExcitations = coneFundamentals * LplusM_SPD_pos;
+contrast = (stimExcitations-backgroundExcitations)./backgroundExcitations
+norm(contrast(1:2))
+% Visual Feild Size Spacing
+fieldSizes = 1:20;
+
+[LminusM_QcmStim] = computeContrastStimWithCIE(ConeDirectedDirections{directionNum}.calibration.describe.S,fieldSizes,bkgrdSPD',LminusM_SPD_pos');
+[LplusM_QcmStim] = computeContrastStimWithCIE(ConeDirectedDirections{directionNum}.calibration.describe.S,fieldSizes,bkgrdSPD',LplusM_SPD_pos');
+
+% Run the QCM for KAS25
+analysisParams = getSubjectParams('KAS25');
+fitOBJ = tfeQCMDirection('verbosity','none','dimension',analysisParams.theDimension);
+
+% Use paramters from the QCM fit to KAS25 measurement set 1
+params.Qvec        = [0.1661 46.1730];
+params.crfAmp      = 2.9998;
+params.crfExponent = 1.4826;
+params.crfSemi     = 0.2817;
+params.expFalloff  = 5.0501; % this does nothing and should be removed
+params.crfOffset   = -0.1737;
+
+% Create the Stimulus Struct for L-M
+stimulusStruct_LminusM.values = LminusM_QcmStim;
+stimulusStruct_LminusM.timebase = 1:length(fieldSizes);
+
+% Create the Stimulus Struct for L-M
+stimulusStruct_LplusM.values = LplusM_QcmStim;
+stimulusStruct_LplusM.timebase = 1:length(fieldSizes);
+
+% Get the HRF
+kernel = [];
+
+% Compute the L-M QCM response
+LminusM_Response = fitOBJ.computeResponse(params,stimulusStruct_LminusM,kernel);
+
+% Compute the L-M QCM response
+LplusM_Response = fitOBJ.computeResponse(params,stimulusStruct_LplusM,kernel);
+
+% Compute the ratio of the L
+LMratio = LminusM_Response.values ./ LplusM_Response.values;
+
+
+
+%% Plot some stuff
 figure;
 subplot(1,3,1)
 plot(LminusM_Response.values,'k')
@@ -151,3 +168,40 @@ plot(LMratio,'k')
 xlabel('Eccentricity')
 ylabel('Ratio')
 title('L-M/L+M Ratio')
+
+% Visual Feild Size Spacing
+fieldSizes = 1:20;
+
+function [QcmStim] = computeContrastStimWithCIE(S,fieldSizes,backgroundSPD,stimulusSPD)
+
+% Loop over visual field sizes
+for ii = 1:length(fieldSizes)
+    
+    % Generate cone fundamentals for different visual field size
+    [~,T_quantal] = ComputeCIEConeFundamentals(S,fieldSizes(ii),30,3);
+    T_energy = EnergyToQuanta(S,T_quantal')';
+    cieConeFund = T_energy./max(T_energy')';
+    
+    % Compute the background activations
+    bkgrdActivations = cieConeFund * backgroundSPD';
+    
+    % compute the stimulus
+    stimActivations = cieConeFund * stimulusSPD';
+
+    % Compute L-M Contrast
+    contrast(:,ii) = (stimActivations - bkgrdActivations) ./bkgrdActivations;
+
+end
+
+% Turn the contrasts from above loop into inputs to the QCM
+% L-M direction
+% Get the angle and contrast
+[theta, contrast] = cart2pol(contrast(1,:),contrast(2,:));
+
+% X and y component for a unit vector in the direction of theta
+[LconesCont, MConesCont] = pol2cart(theta, 1);
+
+% package for the QCM stim
+QcmStim = [LconesCont;MConesCont;contrast];
+
+end
