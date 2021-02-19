e0db68e3f1ca75ad10d239040dcce4a51682c9c2
Michael Barnett <micalan@sas.upenn.edu>
2021-02-19 16:02:27 -0500

    script to make to r^2 map for GLM

--- code/analyzeLFContrast_wholeBrainGLM.m
+++ code/analyzeLFContrast_wholeBrainGLM.m
@@ -13,20 +13,22 @@ templateFile   = fullfile(dropBoxPath,'surfaceMaps','templates','template.dscala
 
 %% Set up stuff
 % Initialize the maps
-rSquaredQcmMap = zeros(91282,1);
+rSquaredGlmMap = zeros(91282,1);
 
-% Create the fit object 
+% Create the fit object
 iampOBJ = tfeIAMP('verbosity','none');
 
 %% Get the cleaned time series
+w = warning ('off','all'); % turn off waring for nuisance regression
 [fullCleanData, analysisParams, voxelIndex] = getTimeCourse_hcp(analysisParams,'wholeBrain',true);
+warning(w)
 % reshape the data to voxels x time point(all 20 runs)
 timeCourses = [];
 for ii = 1:size(fullCleanData,3)
     timeCourses = [timeCourses,fullCleanData(:,:,ii)];
 end
 
-%% Initialize the packet 
+%% Initialize the packet
 % Get a packet for each run (1-20)
 [analysisParams, iampTimeCoursePacketPocket] = generateRunPackets(analysisParams, fullCleanData);
 
@@ -35,57 +37,32 @@ end
 [analysisParams, theConcatPacket] = concatPackets(analysisParams, iampTimeCoursePacketPocket);
 tmpPkt = makeDirectionTimeCoursePacketPocket({theConcatPacket});
 theFullPacket = tmpPkt{1};
-theFullPacket.response.values = []; 
+theFullPacket.response.values = [];
 
-%% loop over voxels 
+%% loop over voxels
 for ii = 1:size(fullCleanData,1)
-
+    
     % add the voxel time course to the packet
     theFullPacket.response.values = timeCourses(ii,:);
     
-    % Fit the time course with the QCM -- { } is because this expects a cell
-    [qcmTcOBJ,qcmParams] = fitDirectionModel(analysisParams, 'qcmFit', {theFullPacket},'fitErrorScalar',1000,'talkToMe',false);
+    % fit the IAMP model
+    defaultParamsInfo.nInstances = size(theFullPacket.stimulus.values,1);
+    [iampParams,fVal,iampResponses] = iampOBJ.fitResponse(theFullPacket,...
+        'defaultParamsInfo', defaultParamsInfo, 'searchMethod','linearRegression');
     
-    % Compute the response 
-    theModelPred = fitOBJ.computeResponse(qcmParams{1},theFullPacket.stimulus,theFullPacket.kernel);
-    qcmCorrVec =  [theFullPacket.response.values',theModelPred.values'];
-    qcmCorrVals = corrcoef(qcmCorrVec(:,1),qcmCorrVec(:,2),'rows','complete').^2;
-
+    % Compute the response
+    
+    glmCorrMat =  [theFullPacket.response.values',iampResponses.values'];
+    glmCorrVals = corrcoef(glmCorrMat(:,1),glmCorrMat(:,2),'rows','complete').^2;
+    
     % Put values back in map
-    qcmParamMap(voxelIndex(ii),:)         = fitOBJ.paramsToVec(qcmParams{1});
-    rSquaredQcmMap(voxelIndex(ii),:)      = qcmCorrVals(1,2);
+    rSquaredGlmMap(voxelIndex(ii),:)      = glmCorrVals(1,2);
 end
 
 
-% write out minor axis ratio
-ciftiVec = qcmParamMap(:,1);
-mapName        = fullfile(mapSavePath,['minorAxisMap_', analysisParams.sessionNickname '.dscalar.nii']);
-makeWholeBrainMap(ciftiVec', [], templateFile, mapName)
-
-% write out minor axis ratio
-ciftiVec = qcmParamMap(:,2);
-mapName        = fullfile(mapSavePath,['angleMap_', analysisParams.sessionNickname '.dscalar.nii']);
-makeWholeBrainMap(ciftiVec', [], templateFile, mapName)
-
-% write out minor axis ratio
-ciftiVec = qcmParamMap(:,3);
-mapName        = fullfile(mapSavePath,['nlAmpMap_', analysisParams.sessionNickname '.dscalar.nii']);
-makeWholeBrainMap(ciftiVec', [], templateFile, mapName)
-
-% write out minor axis ratio
-ciftiVec = qcmParamMap(:,4);
-mapName        = fullfile(mapSavePath,['nlSemiMap_', analysisParams.sessionNickname '.dscalar.nii']);
-makeWholeBrainMap(ciftiVec', [], templateFile, mapName)
-
-% write out minor axis ratio
-ciftiVec = qcmParamMap(:,5);
-mapName        = fullfile(mapSavePath,['nlExpMap_', analysisParams.sessionNickname '.dscalar.nii']);
-makeWholeBrainMap(ciftiVec', [], templateFile, mapName)
-
-% write out mean R squared map
-mapName        = fullfile(mapSavePath,['rSquaredMapQcm_', analysisParams.sessionNickname '.dscalar.nii']);
-makeWholeBrainMap(rSquaredQcmMap', [], templateFile, mapName)
-
+% write out GLM R^2 map
+mapName        = fullfile(mapSavePath,['GLM_', analysisParams.sessionNickname '_wholeBrain.dscalar.nii']);
+makeWholeBrainMap(rSquaredGlmMap', [], templateFile, mapName)
 
 display(['COMPLETED: ',subjId])
 end
