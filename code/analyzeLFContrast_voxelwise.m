55a49af4af43ddc2f0af324a9cc86da21b8bf7de
Michael Barnett <micalan@sas.upenn.edu>
2021-04-07 16:08:05 -0400

    added r squared diff

--- code/analyzeLFContrast_voxelwise.m
+++ code/analyzeLFContrast_voxelwise.m
@@ -12,8 +12,9 @@ templateFile   = fullfile(dropBoxPath,'surfaceMaps','templates','template.dscala
 % Initialize the maps
 qcmParamMap = zeros(91282,7);
 rSquaredQcmMap = zeros(91282,1);
-
-% Create the fit object 
+rSquaredIampMap = zeros(91282,1);
+rSquaredDiffMap = zeros(91282,1);
+% Create the fit object
 fitOBJ = tfeQCMDirection('verbosity','none','dimension',analysisParams.theDimension);
 
 %% Get the cleaned time series
@@ -28,7 +29,7 @@ end
 [analysisParams] = loadHRF(analysisParams);
 
 
-%% Initialize the packet 
+%% Initialize the packet
 % Get a packet for each run (1-20)
 [analysisParams, iampTimeCoursePacketPocket] = generateRunPackets(analysisParams, fullCleanData);
 
@@ -37,25 +38,53 @@ end
 [analysisParams, theConcatPacket] = concatPackets(analysisParams, iampTimeCoursePacketPocket);
 tmpPkt = makeDirectionTimeCoursePacketPocket({theConcatPacket});
 theFullPacket = tmpPkt{1};
-theFullPacket.response.values = []; 
+theFullPacket.response.values = [];
 
-%% loop over voxels 
+% the iamp
+iampOBJ = tfeIAMP('verbosity','none');
+defaultParamsInfo.nInstances = size(theConcatPacket.stimulus.values,1);
+
+%% loop over voxels
 for ii = 1:size(fullCleanData,1)
-
+    
     % add the voxel time course to the packet
     theFullPacket.response.values = timeCourses(ii,:);
+    theConcatPacket.response.values = timeCourses(ii,:);
     
     % Fit the time course with the QCM -- { } is because this expects a cell
     [qcmTcOBJ,qcmParams] = fitDirectionModel(analysisParams, 'qcmFit', {theFullPacket},'fitErrorScalar',1000,'talkToMe',false);
     
-    % Compute the response 
-    theModelPred = fitOBJ.computeResponse(qcmParams{1},theFullPacket.stimulus,theFullPacket.kernel);
-    qcmCorrVec =  [theFullPacket.response.values',theModelPred.values'];
-    qcmCorrVals = corrcoef(qcmCorrVec(:,1),qcmCorrVec(:,2),'rows','complete').^2;
-
+    [iampParams,fVal,iampResponses] = iampOBJ.fitResponse(theConcatPacket,...
+        'defaultParamsInfo', defaultParamsInfo, 'searchMethod','linearRegression');
+    % Compute the response
+    theModelPred = qcmTcOBJ.computeResponse(qcmParams{1},theFullPacket.stimulus,theFullPacket.kernel);
+    
+    %% nan mask
+    if any(isnan(theConcatPacket.response.values))
+        nanMask =  find(isnan(theConcatPacket.response.values));
+        theConcatPacket.response.values(nanMask) = [];
+        theFullPacket.response.values(nanMask) =[];
+        theModelPred.values(nanMask) = [];
+        iampResponses.values(nanMask) = [];
+        
+        qcmCorrVals = corrcoef(theFullPacket.response.values',theModelPred.values').^2;
+        
+        iampCorrVals = corrcoef(theConcatPacket.response.values',iampResponses.values').^2;
+        
+    else
+        
+        qcmCorrVals = corrcoef(theFullPacket.response.values',theModelPred.values').^2;
+        
+        iampCorrVals = corrcoef(theConcatPacket.response.values',iampResponses.values').^2;
+    end
     % Put values back in map
-    qcmParamMap(voxelIndex(ii),:)         = fitOBJ.paramsToVec(qcmParams{1});
+    qcmParamMap(voxelIndex(ii),:)         = qcmTcOBJ.paramsToVec(qcmParams{1});
     rSquaredQcmMap(voxelIndex(ii),:)      = qcmCorrVals(1,2);
+    
+    % Put values back in map
+    rSquaredIampMap(voxelIndex(ii),:)      = iampCorrVals(1,2);
+    
+    rSquaredDiffMap(voxelIndex(ii),:)      = iampCorrVals(1,2)- qcmCorrVals(1,2);
 end
 
 
@@ -64,30 +93,42 @@ ciftiVec = qcmParamMap(:,1);
 mapName        = fullfile(mapSavePath,['minorAxisMap_', analysisParams.sessionNickname '.dscalar.nii']);
 makeWholeBrainMap(ciftiVec', [], templateFile, mapName)
 
-% write out minor axis ratio
+% write out angle
 ciftiVec = qcmParamMap(:,2);
 mapName        = fullfile(mapSavePath,['angleMap_', analysisParams.sessionNickname '.dscalar.nii']);
 makeWholeBrainMap(ciftiVec', [], templateFile, mapName)
 
-% write out minor axis ratio
+% write out amp
 ciftiVec = qcmParamMap(:,3);
 mapName        = fullfile(mapSavePath,['nlAmpMap_', analysisParams.sessionNickname '.dscalar.nii']);
 makeWholeBrainMap(ciftiVec', [], templateFile, mapName)
 
-% write out minor axis ratio
+% write out semi
 ciftiVec = qcmParamMap(:,4);
 mapName        = fullfile(mapSavePath,['nlSemiMap_', analysisParams.sessionNickname '.dscalar.nii']);
 makeWholeBrainMap(ciftiVec', [], templateFile, mapName)
 
-% write out minor axis ratio
+% write out exp
 ciftiVec = qcmParamMap(:,5);
 mapName        = fullfile(mapSavePath,['nlExpMap_', analysisParams.sessionNickname '.dscalar.nii']);
 makeWholeBrainMap(ciftiVec', [], templateFile, mapName)
 
-% write out mean R squared map
+% write out offset
+ciftiVec = qcmParamMap(:,7);
+mapName        = fullfile(mapSavePath,['nlOffset_', analysisParams.sessionNickname '.dscalar.nii']);
+makeWholeBrainMap(ciftiVec', [], templateFile, mapName)
+
+% write out mean QCM R squared map
 mapName        = fullfile(mapSavePath,['rSquaredMapQcm_', analysisParams.sessionNickname '.dscalar.nii']);
 makeWholeBrainMap(rSquaredQcmMap', [], templateFile, mapName)
 
+% write out mean IAMP R squared map
+mapName        = fullfile(mapSavePath,['rSquaredMapIamp_', analysisParams.sessionNickname '.dscalar.nii']);
+makeWholeBrainMap(rSquaredIampMap', [], templateFile, mapName)
+
+% write out mean R squared diff map
+mapName        = fullfile(mapSavePath,['rSquaredDiffMap_', analysisParams.sessionNickname '.dscalar.nii']);
+makeWholeBrainMap(rSquaredDiffMap', [], templateFile, mapName)
 
 display(['COMPLETED: ',subjId])
 end
