6e255e9ee73bb1f0e5919c743d817218856a71dc
Michael Barnett <micalan@sas.upenn.edu>
2021-04-07 16:11:07 -0400

    LCM fitting code

--- code/demos/fitDemoLcm.m
+++ code/demos/fitDemoLcm.m
@@ -3,7 +3,7 @@
 % Description:
 %   This script synthesizes data for the LCM model and then fits it,
 %   to ensure that we can get back what we put in.  Etc.
-% 
+%
 %   The parameters used to synthesize the data were just pulled out of a
 %   hat, so there is no particular expected shape the resulting isoresponse
 %   contour.
@@ -15,7 +15,7 @@
 %% Initialize
 clear all; rng(0);
 % Load the subject relevant info
-analysisParams = getSubjectParams('KAS25');
+analysisParams = getSubjectParams('AP26');
 
 analysisParams.preproc = 'hcp';
 
@@ -60,7 +60,7 @@ LCMObj.paramPrint(fitLCMParams)
 [qcmTcOBJ,qcmParams] = fitDirectionModel(analysisParams, 'qcmFit', timeCoursePacket,'fitErrorScalar',1000,'talkToMe',false);
 
 
-%% Get the time course response 
+%% Get the time course response
 LCMResponseStruct = LCMObj.computeResponse(fitLCMParams,thePacketLcm.stimulus,thePacketLcm.kernel,'AddNoise',false);
 LCMResponseStruct.plotColor = lcmColor;
 [timeCoursePlot.lcm]   = chopUpTimeCourse(LCMResponseStruct,20);
@@ -75,11 +75,24 @@ theTimeCourse{1}.plotColor =[0, 0, 0];
 [timeCoursePlot.timecourse] = chopUpTimeCourse(theTimeCourse{1},20);
 
 
-%% plot it 
- tcHndl = plotTimeCourse(analysisParams, timeCoursePlot, zeros(20,1), 20);
+%% Plot it
+figSavePath =  fullfile(getpref(analysisParams.projectName,'figureSavePath'),analysisParams.expSubjID);
 
+tcHndl = plotTimeCourse(analysisParams, timeCoursePlot, zeros(20,1), 20);
+% Save it
+% Plot configuration
+set(tcHndl, 'Renderer', 'Painters');
+figureSizeInches = [20 15];
+set(tcHndl, 'PaperUnits', 'inches');
+set(tcHndl, 'PaperSize',figureSizeInches);
+set(tcHndl, 'PaperPosition', [0 0 figureSizeInches(1) figureSizeInches(2)]);
+% Full file name
+figNameTc =  fullfile(figSavePath,[analysisParams.expSubjID,'_LCM_TimeCourse_' analysisParams.sessionNickname...
+    '_' analysisParams.preproc '.pdf']);
+% Save it
+print(tcHndl, figNameTc, '-dpdf', '-r300');
 %% Get and plot isoresponse contour
-figure; hold on 
+ellipseHndl = figure; hold on
 [isoContrastFit,unitContrastResponseFit,angleSupport] = LCMObj.getIsoContrast(fitLCMParams);
 plot(isoContrastFit.*cosd(angleSupport),isoContrastFit.*sind(angleSupport),'r','LineWidth',2, 'color', lcmColor);
 axis('square');
@@ -105,6 +118,13 @@ line([0 0], [-2 2], 'Color', [.3 .3 .3], 'LineStyle', ':','LineWidth', 2);
 
 legend('LCM','QCM')
 
-
+set(ellipseHndl, 'Renderer', 'Painters');
+figureSizeInches = [7 6];
+set(ellipseHndl, 'PaperUnits', 'inches');
+set(ellipseHndl, 'PaperSize',figureSizeInches);
+set(ellipseHndl, 'PaperPosition', [0 0 figureSizeInches(1) figureSizeInches(2)]);
+figNameEllipseNonlin = fullfile(figSavePath,[analysisParams.expSubjID,'_LCM_Ellipse_' ...
+    analysisParams.sessionNickname '_' analysisParams.preproc '.pdf']);
+print(ellipseHndl, figNameEllipseNonlin, '-dpdf', '-r300');
 
 
