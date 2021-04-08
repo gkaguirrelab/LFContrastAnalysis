e12a82ac72b3ab4591be0f236893609d7e081993
Michael Barnett <micalan@sas.upenn.edu>
2021-04-08 13:17:52 -0400

    added contour scaling

--- code/demos/fitDemoLcm.m
+++ code/demos/fitDemoLcm.m
@@ -14,6 +14,10 @@
 
 %% Initialize
 clear all; rng(0);
+% Fit error scalar.  Big value seems
+% to work better here.
+fitErrorScalar = 10000;
+
 % Load the subject relevant info
 analysisParams = getSubjectParams('AP26');
 
@@ -53,11 +57,25 @@ iampOBJ = tfeIAMP('verbosity','none');
 LCMObj = tfeLCMDirection('verbosity','none','dimension',theDimension);
 
 % Fit the LCM packet
-[fitLCMParams,fVal,fitResponseStructLCM] = LCMObj.fitResponse(thePacketLcm);
+[fitLCMParams,fVal,fitResponseStructLCM] =LCMObj.fitResponse(thePacketLcm,'fitErrorScalar',fitErrorScalar,...
+    'maxIter',3000,'maxFunEval',3000); 
 fprintf('\nLCM parameters from fit:\n');
 LCMObj.paramPrint(fitLCMParams)
 % Fit the time course with the QCM -- { } is because this expects a cell
 [qcmTcOBJ,qcmParams] = fitDirectionModel(analysisParams, 'qcmFit', timeCoursePacket,'fitErrorScalar',1000,'talkToMe',false);
+
+%% Generate an ellipsoidal isoresponse contour
+ellipticalIsoContrast = tfeEllipticalIsoContrast(qcmParams{1}.Qvec(2),qcmParams{1}.Qvec(1),LCMObj.angleSupport,LCMObj.criterionResp);
+
+%% Get parameters with model parameters scaled to produce best fit to elliptical contour
+%
+% This shows how to scale an isoresponse contour, but doesn't try to adjust
+% relative channel weights to best fit the ellipse.
+newCriterionResp = LCMObj.scaleToFitIsoContrast(fitLCMParams,ellipticalIsoContrast);
+saveCriterionResp = LCMObj.criterionResp;
+LCMObj.criterionResp = newCriterionResp;
+scaledToEllipseIsoContrast = LCMObj.getIsoContrast(fitLCMParams);
+LCMObj.criterionResp = saveCriterionResp;
 
 
 %% Get the time course response
@@ -75,7 +93,7 @@ theTimeCourse{1}.plotColor =[0, 0, 0];
 [timeCoursePlot.timecourse] = chopUpTimeCourse(theTimeCourse{1},20);
 
 
-%% Plot it
+%% Plot it time course
 figSavePath =  fullfile(getpref(analysisParams.projectName,'figureSavePath'),analysisParams.expSubjID);
 
 tcHndl = plotTimeCourse(analysisParams, timeCoursePlot, zeros(20,1), 20);
@@ -91,15 +109,24 @@ figNameTc =  fullfile(figSavePath,[analysisParams.expSubjID,'_LCM_TimeCourse_' a
     '_' analysisParams.preproc '.pdf']);
 % Save it
 print(tcHndl, figNameTc, '-dpdf', '-r300');
+
+
 %% Get and plot isoresponse contour
+% ellipseHndl = figure; hold on
+% [isoContrastFit,unitContrastResponseFit,angleSupport] = LCMObj.getIsoContrast(fitLCMParams);
+% plot(isoContrastFit.*cosd(angleSupport),isoContrastFit.*sind(angleSupport),'r','LineWidth',2, 'color', lcmColor);
+% axis('square');
+% xlim([-2 2]); ylim([-2 2]);
+% xlabel('Cone 1 Contrast');
+% ylabel('Cone 2 Contrast');
+% title('IsoContrast');
 ellipseHndl = figure; hold on
-[isoContrastFit,unitContrastResponseFit,angleSupport] = LCMObj.getIsoContrast(fitLCMParams);
-plot(isoContrastFit.*cosd(angleSupport),isoContrastFit.*sind(angleSupport),'r','LineWidth',2, 'color', lcmColor);
-axis('square');
-xlim([-2 2]); ylim([-2 2]);
-xlabel('Cone 1 Contrast');
-ylabel('Cone 2 Contrast');
-title('IsoContrast');
+isoContrastLCM = LCMObj.getIsoContrast(fitLCMParams);
+%plot(ellipticalIsoContrast.*cosd(LCMObj.angleSupport),ellipticalIsoContrast.*sind(LCMObj.angleSupport),'k','LineWidth',5);
+plot(isoContrastLCM.*cosd(LCMObj.angleSupport),isoContrastLCM.*sind(LCMObj.angleSupport),'r','LineWidth',3);
+plot(scaledToEllipseIsoContrast.*cosd(LCMObj.angleSupport),scaledToEllipseIsoContrast.*sind(LCMObj.angleSupport),'b','LineWidth',2);
+
+
 %% Ellipse Figure
 nQCMPoints = 200;
 % Calculate the Minv matrix to tranform a unit circle to the ellipse and do it
@@ -110,14 +137,17 @@ ellipsePoints = Minv*circlePoints;
 axh = gca;
 
 % plot ellipse
-line(ellipsePoints(1,:),ellipsePoints(2,:),'color', qcmColor, 'LineWidth', 2);
+plot(ellipsePoints(1,:),ellipsePoints(2,:),'color', qcmColor, 'LineWidth', 2);
+axis('square');
+xlim([-2 2]); ylim([-2 2]);
+xlabel('Cone 1 Contrast');
+ylabel('Cone 2 Contrast');
+title('IsoContrast');
 
 % plot axes
 line([-2 2], [0 0], 'Color', [.3 .3 .3], 'LineStyle', ':','LineWidth', 2);
 line([0 0], [-2 2], 'Color', [.3 .3 .3], 'LineStyle', ':','LineWidth', 2);
-
-legend('LCM','QCM')
-
+legend('LCM', 'LCM Scaled To Fit','Ellipse');
 set(ellipseHndl, 'Renderer', 'Painters');
 figureSizeInches = [7 6];
 set(ellipseHndl, 'PaperUnits', 'inches');
